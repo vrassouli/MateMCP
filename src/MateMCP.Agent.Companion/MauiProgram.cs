90d5ba4e89e3ce74f8b4dd7dbbcb2b727b5d7077
Vahid Rassouli <vrassouli@gmail.com>
2026-09-01 16:59:01 +0330

    Register Companion native notification services

--- src/MateMCP.Agent.Companion/MauiProgram.cs
+++ src/MateMCP.Agent.Companion/MauiProgram.cs
@@ -13,6 +13,8 @@ public static class MauiProgram
         builder.Services.AddMauiBlazorWebView();
         builder.Services.AddBluentUI();
         builder.Services.AddSingleton<AgentApiClient>();
+        builder.Services.AddSingleton<NativeApprovalNotifier>();
+        builder.Services.AddSingleton<ApprovalNotificationWatcher>();
 
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
